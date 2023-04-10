5342ae0f4c5a67ae52207c07f18e5cd1697d59a6
angelperdomo95 <mi_lo2009@hotmail.es>
2023-04-10 03:05:14 -0500

    agregar git ignore y primeros archos

---
+++ script.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btncalcular");
+const Presult = document.querySelector("#result");
+
+btn.addEventListener("submit", sumarInputValues);
+
+function sumarInputValues(event) {
+    //console.log({event});
+    event.preventDefault();
+    const sumaInputs = input1.value + input2.value;
+    Presult.innerText = "Resultado: " + sumaInputs;}
